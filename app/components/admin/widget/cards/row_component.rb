40ede05d170d481018df3c292f87a0fa33b58436
taitus <sebastia.roig@gmail.com>
2023-10-27 11:00:08 +0200

    Only render position field on table when cards rendered are not headers

--- app/components/admin/widget/cards/row_component.rb
+++ app/components/admin/widget/cards/row_component.rb
@@ -1,8 +1,10 @@
 class Admin::Widget::Cards::RowComponent < ApplicationComponent
-  attr_reader :card, :options
+  attr_reader :card, :header_section, :options
+  alias_method :header_section?, :header_section
 
-  def initialize(card, **options)
+  def initialize(card, header_section, **options)
     @card = card
+    @header_section = header_section
     @options = options
   end
 end
--- app/components/admin/widget/cards/table_component.rb
+++ app/components/admin/widget/cards/table_component.rb
@@ -1,8 +1,10 @@
 class Admin::Widget::Cards::TableComponent < ApplicationComponent
-  attr_reader :cards, :no_cards_message, :options
+  attr_reader :cards, :header_section, :no_cards_message, :options
+  alias_method :header_section?, :header_section
 
-  def initialize(cards, no_cards_message:, **options)
+  def initialize(cards, header_section: false, no_cards_message:, **options)
     @cards = cards
+    @header_section = header_section
     @no_cards_message = no_cards_message
     @options = options
   end
