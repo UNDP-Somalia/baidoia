de5c3ab224715fbebe15c51a238f7d111ca91af6
taitus <sebastia.roig@gmail.com>
2023-10-27 10:53:16 +0200

    Allow saving a position when create a widget card
    We are ensuring that only position field is rendered only on
    non-header cards.
    
    Note that we have 3 sections that use widget cards:
    - Homepage (cards and header cards)
    - Custompages (only have cards)
    - Sdg Homepage (cards and header cards)

--- spec/system/admin/widgets/cards_spec.rb
+++ spec/system/admin/widgets/cards_spec.rb
@@ -11,7 +11,8 @@ describe "Cards", :admin do
     fill_in "Title", with: "Card text"
     fill_in "Description", with: "Card description"
     fill_in "Link text", with: "Link text"
-    fill_in "widget_card_link_url", with: "consul.dev"
+    fill_in "Link URL", with: "consul.dev"
+    fill_in "Position", with: "12"
     attach_image_to_card
     click_button "Create card"
 
@@ -25,6 +26,7 @@ describe "Cards", :admin do
         expect(page).to have_content "Card description"
         expect(page).to have_content "Link text"
         expect(page).to have_content "consul.dev"
+        expect(page).to have_css("td", exact_text: "12")
         expect(page).to have_link "Show image", title: "clippy.jpg"
       end
     end
@@ -85,7 +87,8 @@ describe "Cards", :admin do
       fill_in "Link text", with: "Link text updated"
     end
 
-    fill_in "widget_card_link_url", with: "consul.dev updated"
+    fill_in "Link URL", with: "consul.dev updated"
+    fill_in "Position", with: "2"
     click_button "Save card"
 
     expect(page).to have_content "Card updated successfully"
@@ -99,6 +102,7 @@ describe "Cards", :admin do
         expect(page).to have_content "Card description updated"
         expect(page).to have_content "Link text updated"
         expect(page).to have_content "consul.dev updated"
+        expect(page).to have_css("td", exact_text: "2")
       end
     end
   end
@@ -122,6 +126,8 @@ describe "Cards", :admin do
     scenario "Create" do
       visit admin_homepage_path
       click_link "Create header"
+
+      expect(page).not_to have_selector("label", text: "Position")
 
       fill_in "Label (optional)", with: "Header label"
       fill_in "Title", with: "Header text"
@@ -172,10 +178,13 @@ describe "Cards", :admin do
 
         fill_in "Title", with: "Card for a custom page"
         fill_in "Link URL", with: "/any_path"
+        fill_in "Position", with: "12"
         click_button "Create card"
 
         expect(page).to have_current_path admin_site_customization_page_widget_cards_path(custom_page)
         expect(page).to have_content "Card for a custom page"
+        expect(page).to have_content "12"
+        expect(page).to have_css("th", exact_text: "Position")
       end
 
       scenario "Show" do
@@ -237,12 +246,14 @@ describe "Cards", :admin do
         within(".translatable-fields") do
           fill_in "Title", with: "Updated title"
         end
+        fill_in "Position", with: "2"
 
         click_button "Save card"
 
         expect(page).to have_current_path admin_site_customization_page_widget_cards_path(custom_page)
         expect(page).to have_content "Updated title"
         expect(page).not_to have_content "Original title"
+        expect(page).to have_css("td", exact_text: "2")
       end
 
       scenario "Destroy" do
