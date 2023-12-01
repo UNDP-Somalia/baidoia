a259a71eed8b0e14c8e5071c76e74049fc7a095f
Diego Calvo Castillo <diegocc6@gmail.com>
2023-12-01 13:57:02 +0100

    fields in registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_23_090028) do
+ActiveRecord::Schema.define(version: 2023_12_01_084047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -1645,6 +1645,7 @@ ActiveRecord::Schema.define(version: 2023_05_23_090028) do
     t.boolean "recommended_debates", default: true
     t.boolean "recommended_proposals", default: true
     t.string "subscriptions_token"
+    t.string "last_education_degree"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["date_of_birth"], name: "index_users_on_date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
